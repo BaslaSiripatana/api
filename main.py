dc3cdc792704ff5444600c3f3064347c848d8da1
BaslaSiripatana <79196014+BaslaSiripatana@users.noreply.github.com>
2024-03-09 20:48:00 +0700

    1

--- main.py
+++ main.py
@@ -1,58 +1,3 @@
-# from fastapi import FastAPI,UploadFile, File
-# import uvicorn
-# from pydantic import BaseModel
-# from fastapi.middleware.cors import CORSMiddleware
-# import os 
-
-# app = FastAPI()
-
-# # เพิ่ม middleware เพื่ออนุญาตให้เข้าถึงจาก origin ของคุณ
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000"],
-#     allow_credentials=True,
-#     allow_methods=["POST"],
-#     allow_headers=["*"],
-# )
-
-# class PredictionResult(BaseModel):
-#     predictions: str
-#     accuracy: float
-
-# @app.get("/")
-# def hello():
-#     return {"API":"API is working fine"}
-
-# @app.post("/predict")
-# async def upload_image(img_file:UploadFile =File(...)):
-
-#     if '.jpg' in img_file.filename or '.jpeg' in img_file.filename or '.png' in img_file.filename:
-#         file_save_path="./images/"+img_file.filename
-#         if os.path.exists("./images") == False:
-#             os.makedirs("./images")
-
-#         with open(file_save_path, "wb") as f:
-#             f.write(img_file.file.read())
-
-#         if os.path.exists(file_save_path):
-#             # return {"image_path":file_save_path,"message": "Image saved successfully"}
-
-#             predictions = "normal"
-#             accuracy = 100
-
-#             result = PredictionResult(predictions=predictions, accuracy=accuracy)
-#             return result
-#         else:
-#             return {"error":"Image Not saved !!!"}
-#     else:
-#         return {"error": "File Type is not valid please upload only jpg,jpeg and png"}
-
-# if __name__=="__main__":
-#     uvicorn.run(app)
-
-
-
-
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
 from pydantic import BaseModel
@@ -91,7 +36,7 @@ async def upload_image(img_file: UploadFile = File(...)):
 
         if os.path.exists(file_save_path):
             # สร้างข้อมูลการทำนาย
-            result = PredictionResult(predictions="normal", accuracy=100)
+            result = PredictionResult(predictions="violence", accuracy=100)
             # ส่งข้อมูลการทำนายกลับไปยังไคลเอนต์
             return result
     return {"error": "Invalid image format"}
